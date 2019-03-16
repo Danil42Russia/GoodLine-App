6e591900e0f3207f5c60b3ae03ec4aa15eda2c77
Danil42Russia <dan.dan.d@yandex.ru>
2019-03-16 04:48:43 +0000

    Functional refactoring

--- src/ru/danil42russia/aaa/Main.kt
+++ src/ru/danil42russia/aaa/Main.kt
@@ -2,12 +2,13 @@ package ru.danil42russia.aaa
 
 //Ctrl+J
 //Ctrl+Shift+F12
+//Ctrl+Shift+J
+//Ctrl+Shift+L
+//Ctrl+Shift+O
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("No args!!!")
     } else {
-        for (arg in args) {
-            println(arg)
-        }
+        args.forEach { println(it) }
     }
 }
