3854eec47c6ee944b37d5abb2633c34be75c35f5
Danil42Russia <dan.dan.d@yandex.ru>
2019-03-23 05:29:00 +0000

    Create getStatus with callback

--- src/ru/danil42russia/aaa/Main.kt
+++ src/ru/danil42russia/aaa/Main.kt
@@ -9,18 +9,17 @@ package ru.danil42russia.aaa
  */
 
 fun main(args: Array<String>) {
-    val status= when {
-        args.isEmpty() -> {
-            println("No args!!!")
-            0
-        }
-        args.size == 1 -> 1
-        args.size == 2 -> 2
-        else -> {
-            args.forEach { println(it) }
-            100
-        }
+    val status = when (args.size) {
+        0 -> getStatus(0, { println("No args!!!") })
+        1 -> getStatus(1, {})
+        2 -> getStatus(2, {})
+        else -> getStatus(100, { args.forEach { println(it) } })
     }
 
     System.exit(status)
+}
+
+fun getStatus(status: Int, callback: () -> Unit): Int {
+    callback()
+    return status
 }
