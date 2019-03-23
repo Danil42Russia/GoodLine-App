461ef966ed710ec1ec6e55eae3e5b3c7f12b7653
Danil42Russia <dan.dan.d@yandex.ru>
2019-03-23 04:14:23 +0000

    Add test

--- TEST.sh
+++ TEST.sh
@@ -2,12 +2,17 @@
 clear
 ./BUILD.sh
 
+COUNT_ALL=0
+COUNT_SUC=0
+
 echo
 echo NO args
 ./RUN.sh
 RES=$?
-if [ $? -eq 0 ]; then
+let COUNT_ALL++
+if [ $RES -eq 0 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 0 Actual $RES
 fi
@@ -16,8 +21,10 @@ echo
 echo 1 args
 ./RUN.sh foo
 RES=$?
-if [ $? -eq 1 ]; then
+let COUNT_ALL++
+if [ $RES -eq 1 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 1 Actual $RES
 fi
@@ -26,8 +33,10 @@ echo
 echo 2 args
 ./RUN.sh foo bar
 RES=$?
-if [ $? -eq 2 ]; then
+let COUNT_ALL++
+if [ $RES -eq 2 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 2 Actual $RES
 fi
@@ -36,8 +45,18 @@ echo
 echo Many args
 ./RUN.sh foo bar baz "1 2 3"
 RES=$?
-if [ $? -eq 100 ]; then
+let COUNT_ALL++
+if [ $RES -eq 100 ]; then
     echo Test passed
+    let "COUNT_SUC = $COUNT_SUC + 1"
 else
     echo Test failed. Expected 100 Actual $RES
+fi
+
+if [ $COUNT_ALL -eq $COUNT_SUC ]; then
+    echo All $COUNT_SUC test passed
+    exit 0
+else
+    echo $COUNT_SUC success test out $COUNT_ALL
+    exit 1
 fi
