c022b9586258019bddb5b51300cc4d7acc40fb5f
Danil42Russia <dan.dan.d@yandex.ru>
2019-03-23 05:34:59 +0000

    Add default callback value

--- src/ru/danil42russia/aaa/Main.kt
+++ src/ru/danil42russia/aaa/Main.kt
@@ -10,16 +10,20 @@ package ru.danil42russia.aaa
 
 fun main(args: Array<String>) {
     val status = when (args.size) {
-        0 -> getStatus(0, { println("No args!!!") })
-        1 -> getStatus(1, {})
-        2 -> getStatus(2, {})
-        else -> getStatus(100, { args.forEach { println(it) } })
+        0 -> getStatus(0) {
+            println("No args!!!")
+        }
+        1 -> getStatus(1)
+        2 -> getStatus(2)
+        else -> getStatus(100) {
+            args.forEach { println(it) }
+        }
     }
 
     System.exit(status)
 }
 
-fun getStatus(status: Int, callback: () -> Unit): Int {
+fun getStatus(status: Int, callback: () -> Unit = {}): Int {
     callback()
     return status
 }
