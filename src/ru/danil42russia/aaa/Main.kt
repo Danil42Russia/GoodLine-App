a135bd3ef107c6d5c6331ba66156e9aeba2b3cc3
Danil42Russia <dan.dan.d@yandex.ru>
2019-03-23 04:52:05 +0000

    Add exit-codes

--- src/ru/danil42russia/aaa/Main.kt
+++ src/ru/danil42russia/aaa/Main.kt
@@ -11,7 +11,13 @@ package ru.danil42russia.aaa
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         println("No args!!!")
+        System.exit(0)
+    } else if (args.size == 1) {
+        System.exit(1)
+    } else if (args.size == 2) {
+        System.exit(2)
     } else {
         args.forEach { println(it) }
+        System.exit(100)
     }
 }
