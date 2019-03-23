15013010386da9d842f7802ad72906e0f1e3e031
Danil42Russia <dan.dan.d@yandex.ru>
2019-03-23 05:10:41 +0000

    Refactoring exit-codes

--- src/ru/danil42russia/aaa/Main.kt
+++ src/ru/danil42russia/aaa/Main.kt
@@ -9,16 +9,19 @@ package ru.danil42russia.aaa
  */
 
 fun main(args: Array<String>) {
+    val status: Int
     when {
         args.isEmpty() -> {
             println("No args!!!")
-            System.exit(0)
+            status = 0
         }
-        args.size == 1 -> System.exit(1)
-        args.size == 2 -> System.exit(2)
+        args.size == 1 -> status = 1
+        args.size == 2 -> status = 2
         else -> {
             args.forEach { println(it) }
-            System.exit(100)
+            status = 100
         }
     }
+
+    System.exit(status)
 }
