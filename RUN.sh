90eb28ab398cb58946608696b37b1a064c5c4e5c
Danil42Russia <dan.dan.d@yandex.ru>
2019-04-17 17:33:53 +0700

    Merge branch 'master' of C:\Users\danda\source\repos\Dev\GoodLine-App with conflicts.

--- RUN.sh
+++ RUN.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -classpath "libs/commons-cli-1.4.jar:out/aaa.jar" ru.danil42russia.aaa.MainKt "$@"
+java -classpath libs/commons-cli-1.4.jar;out/aaa.jar ru.danil42russia.aaa.MainKt "$@"
