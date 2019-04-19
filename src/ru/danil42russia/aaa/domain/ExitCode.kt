73fc4fa7ae3278e7238cc0127ff23d63d1b31d80
Danil42Russia <dan.dan.d@yandex.ru>
2019-04-19 23:05:27 +0700

    Renamed const to coding conventions #2

--- src/ru/danil42russia/aaa/domain/ExitCode.kt
+++ src/ru/danil42russia/aaa/domain/ExitCode.kt
@@ -1,13 +1,13 @@
 package ru.danil42russia.aaa.domain
 
 enum class ExitCode(val code: Int) {
-    NOAUTH(-1),
+    NO_AUTH(-1),
     SUCCESS(0),
     HELP(1),
-    BADLOGINFORMAT(2),
-    BADLOGIN(3),
-    BADPASSWORD(4),
-    BADROLE(5),
-    NOTPERMISSION(6),
-    INCORRECTACTIVITY(7),
+    BAD_LOGIN_FORMAT(2),
+    BAD_LOGIN(3),
+    BAD_PASSWORD(4),
+    BAD_ROLE(5),
+    NOT_PERMISSION(6),
+    INCORRECT_ACTIVITY(7),
 }
