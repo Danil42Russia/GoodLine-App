0f93b8d5e6c79e0b776543ccd75eae1813df4b8e
Danil42Russia <dan.dan.d@yandex.ru>
2019-04-24 18:35:54 +0700

    add javadoc to the fun SHA256 + refactoring

--- src/ru/danil42russia/aaa/SHA256.kt
+++ src/ru/danil42russia/aaa/SHA256.kt
@@ -3,14 +3,23 @@ package ru.danil42russia.aaa
 import java.math.BigInteger
 import java.security.MessageDigest
 
+/**
+ * Hashes a string using SHA-256
+ *
+ *
+ *
+ * @param text
+ *
+ * @return hashed string
+ */
 fun SHA256(text: String): String {
+    //Указываем какой алгоритм использовать
     val md = MessageDigest.getInstance("SHA-256")
+    //Вычисляем дайджест
     val messageDigest = md.digest(text.toByteArray())
+    //Преобразуем байтовый массив в число
     val no = BigInteger(1, messageDigest)
-    var hashText = no.toString(16)
-    while (hashText.length < 32) {
-        hashText = "0$hashText"
-    }
 
-    return hashText
+    //Возвращаем в шестнадцатеричном формате
+    return no.toString(16)
 }
