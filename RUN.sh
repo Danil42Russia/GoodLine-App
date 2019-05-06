91b4d0da55f5e425e4bc29b69548e61ba1692c42
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-07 00:05:29 +0700

    Add libs Apache Log4j API and Apache Log4j Core

--- RUN.sh
+++ RUN.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -classpath "libs/commons-cli-1.4.jar:out/aaa.jar" ru.danil42russia.aaa.MainKt "$@"
+java -classpath "libs/commons-cli-1.4.jar:libs/log4j-api-2.11.2.jar:libs/log4j-core-2.11.2.jar:out/aaa.jar" ru.danil42russia.aaa.MainKt "$@"
