510d3fc875a673e279f9e219ddfddfc63514d03a
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-07 16:23:54 +0700

    Add path to the folder. Add logging

--- src/main/kotlin/ru/danil42russia/aaa/service/MigrationService.kt
+++ src/main/kotlin/ru/danil42russia/aaa/service/MigrationService.kt
@@ -1,15 +1,26 @@
 package ru.danil42russia.aaa.service
 
+import org.apache.logging.log4j.LogManager
 import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.FlywayException
 
 class MigrationService {
+    private val log = LogManager.getLogger(BusinessLogic::class.java)
     private val flyway: Flyway = Flyway()
 
     init {
         flyway.setDataSource("jdbc:sqlite:aaa.db", "", "")
+        flyway.setLocations("filesystem:src/main/resources/db/migration")
     }
 
     fun migrate() {
-        flyway.migrate()
+        log.debug("Start migration")
+
+        try {
+            flyway.migrate()
+            log.debug("Migration successful")
+        } catch (ex: FlywayException) {
+            log.debug("Migration failed $ex")
+        }
     }
 }
