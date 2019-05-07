d1e3ecf4d5e5076000d7031b94b4c6a331a8c7d2
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-07 17:38:27 +0700

    Create class DBService

---
+++ src/main/kotlin/ru/danil42russia/aaa/service/DBService.kt
@@ -0,0 +1,32 @@
+package ru.danil42russia.aaa.service
+
+import org.apache.logging.log4j.LogManager
+import java.sql.Connection
+import java.sql.DriverManager
+
+class DBService {
+    private val log = LogManager.getLogger(BusinessLogic::class.java)
+    private var connection: Connection? = null
+
+    init {
+        log.debug("Connection start")
+        Class.forName("org.sqlite.JDBC")
+
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:aaa.db")
+            log.debug("Connection successful")
+        } catch (ex: Exception) {
+            log.debug("Connection failed $ex")
+            connection = null
+        }
+
+    }
+
+    fun getConnection(): Connection? {
+        return connection
+    }
+
+    fun close() {
+        connection?.close()
+    }
+}
