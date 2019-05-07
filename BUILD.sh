cf3ccba9afc12b1cdf2f0a96e0fd884466ed469a
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-07 14:49:13 +0700

    Add lib Flyway Core

--- BUILD.sh
+++ BUILD.sh
@@ -2,4 +2,4 @@
 rm -rf out
 mkdir out
 
-kotlinc -d out/aaa.jar -include-runtime src -cp "libs/commons-cli-1.4.jar:libs/log4j-api-2.11.2.jar:libs/log4j-core-2.11.2.jar"
+kotlinc -d out/aaa.jar -include-runtime src -cp "libs/commons-cli-1.4.jar:libs/log4j-api-2.11.2.jar:libs/log4j-core-2.11.2.jar:libs/flyway-core-5.2.4.jar"
