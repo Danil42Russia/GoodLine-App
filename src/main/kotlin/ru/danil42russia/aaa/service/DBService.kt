4832daf4d82fdb7e1e68871b17cb1e27af0c8bd2
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-09 21:30:49 +0700

    Edit type exception #3

--- src/main/kotlin/ru/danil42russia/aaa/service/DBService.kt
+++ src/main/kotlin/ru/danil42russia/aaa/service/DBService.kt
@@ -3,6 +3,7 @@ package ru.danil42russia.aaa.service
 import org.apache.logging.log4j.LogManager
 import java.sql.Connection
 import java.sql.DriverManager
+import java.sql.SQLException
 
 class DBService {
     private val log = LogManager.getLogger(BusinessLogic::class.java)
@@ -19,7 +20,7 @@ class DBService {
                 System.getenv("DBPASSWORD")
             )
             log.debug("Connection successful")
-        } catch (ex: Exception) {
+        } catch (ex: SQLException) {
             log.debug("Connection failed $ex")
             connection = null
         } finally {
