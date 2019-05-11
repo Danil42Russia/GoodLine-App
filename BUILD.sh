4f3d6819f85872e9024005e811fe340202ffc0b0
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-11 22:33:10 +0700

    Delete olf file

--- BUILD.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-rm -rf out
-mkdir out
-
-kotlinc -d out/aaa.jar -include-runtime src -cp "libs/commons-cli-1.4.jar:libs/log4j-api-2.11.2.jar:libs/log4j-core-2.11.2.jar:libs/flyway-core-4.2.0.jar:libs/sqlite-jdbc-3.27.2.jar"
--- RUN.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -Dlog4j.configurationFile=src/main/resources/log4j2.xml -classpath "libs/commons-cli-1.4.jar:libs/log4j-api-2.11.2.jar:libs/log4j-core-2.11.2.jar:libs/flyway-core-4.2.0.jar:libs/sqlite-jdbc-3.27.2.jar:out/aaa.jar" ru.danil42russia.aaa.MainKt "$@"
