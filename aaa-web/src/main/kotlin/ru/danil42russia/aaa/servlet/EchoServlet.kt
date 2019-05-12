af46c9ae0425a07eab007e2b61f9d4c00577b611
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-13 01:17:40 +0700

    Add request parameter

--- aaa-web/src/main/kotlin/ru/danil42russia/aaa/servlet/EchoServlet.kt
+++ aaa-web/src/main/kotlin/ru/danil42russia/aaa/servlet/EchoServlet.kt
@@ -1,24 +1,17 @@
 package ru.danil42russia.aaa.servlet
 
-import javax.servlet.ServletException
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+
 class EchoServlet : HttpServlet() {
 
     override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
-
-        try {
-            req?.getRequestDispatcher("/echo.jsp")?.forward(req, resp)
-        } catch (ex: ServletException) {
-        }
+        servletContext?.getRequestDispatcher("/echo.jsp")?.forward(req, resp)
     }
 
     override fun doPost(req: HttpServletRequest?, resp: HttpServletResponse?) {
-        try {
-            req?.getRequestDispatcher("/echo.jsp")?.forward(req, resp)
-        } catch (ex: ServletException) {
-        }
+        req?.getRequestDispatcher("/echo.jsp")?.forward(req, resp)
     }
 }
--- aaa-web/src/main/webapp/echo.jsp
+++ aaa-web/src/main/webapp/echo.jsp
@@ -1,9 +1,11 @@
-    <!DOCTYPE html>
+    <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+        <!DOCTYPE html>
         <html>
         <head>
         <title>Main Servlet</title>
         </head>
         <body>
+        <p>${pageContext.request.getParameter("X")}</p>
         <p>MainServlet</p>
         </body>
         </html>
