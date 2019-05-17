7e56398eb9216a43ada6b9bea98c41bd71996ddf
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-17 23:29:38 +0700

    Serialization domain class User

--- aaa-app/src/main/kotlin/ru/danil42russia/aaa/domain/User.kt
+++ aaa-app/src/main/kotlin/ru/danil42russia/aaa/domain/User.kt
@@ -1,8 +1,14 @@
 package ru.danil42russia.aaa.domain
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+
+@Serializable
 data class User(
     val id: Int,
     val login: String,
+    @Transient
     val pass: String = "",
+    @Transient
     val salt: String = ""
 )
