5fa26b1602aea7fb79c4acd33ee0fdba401729b6
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-17 22:38:33 +0700

    Add migration to web subproject

--- aaa-web/src/main/kotlin/ru/danil42russia/aaa/guice/GuiceServletConfig.kt
+++ aaa-web/src/main/kotlin/ru/danil42russia/aaa/guice/GuiceServletConfig.kt
@@ -4,12 +4,16 @@ import com.google.inject.Guice
 import com.google.inject.Injector
 import com.google.inject.servlet.GuiceServletContextListener
 import org.apache.logging.log4j.LogManager
+import ru.danil42russia.aaa.service.MigrationService
 
 class GuiceServletConfig : GuiceServletContextListener() {
     private val log = LogManager.getLogger(GuiceServletConfig ::class.java)
 
     override fun getInjector(): Injector {
         log.debug("Init Guice")
+        val migrationService = MigrationService()
+        migrationService.migrate()
+
         return Guice.createInjector(ServletStages(), ModuleStages())
     }
 }
