a9785b2302423a91a86c45e5f757e0ff71303f32
Danil42Russia <dan.dan.d@yandex.ru>
2019-05-28 10:58:08 +0700

    Add annotations to domain classes

--- aaa-app/src/main/kotlin/ru/danil42russia/aaa/domain/data/User.kt
+++ aaa-app/src/main/kotlin/ru/danil42russia/aaa/domain/data/User.kt
@@ -2,13 +2,25 @@ package ru.danil42russia.aaa.domain.data
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import javax.persistence.*
 
 @Serializable
+@Entity
+@Table(name = "users")
 data class User(
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     val id: Int,
+
+    @Column(name = "login")
     val login: String,
+
+    @Column(name = "pass")
     @Transient
     val pass: String = "",
+
+    @Column(name = "salt")
     @Transient
     val salt: String = ""
 )
